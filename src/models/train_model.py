5c9abff476df952b0987b69606638d8d1f131c0a
Amit Gurkhe <amitg1993@gmail.com>
2024-01-27 09:18:34 +0530

    updated requirements and params

---
+++ src/models/train_model.py
@@ -0,0 +1,13 @@
+# train_model.py
+import pathlib
+import sys
+import joblib
+import mlflow
+
+import pandas as pd
+from hyperopt import hp
+from sklearn.model_selection import train_test_split
+from hyperopt.pyll.base import scope
+from sklearn.metrics import mean_squared_error
+from hyperopt import hp, fmin, tpe, Trials, STATUS_OK, space_eval
+from xgboost import XGBRegressor

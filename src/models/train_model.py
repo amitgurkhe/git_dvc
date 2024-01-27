32eefc950a6f300f9fab53d358d5d7cfaa67e0d9
Amit Gurkhe <amitg1993@gmail.com>
2024-01-27 12:41:43 +0530

    added train model code, added the same in pipeline and updated params.yaml

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -8,6 +8,99 @@ import pandas as pd
 from hyperopt import hp
 from sklearn.model_selection import train_test_split
 from hyperopt.pyll.base import scope
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import f1_score, precision_score, accuracy_score, roc_auc_score
 from hyperopt import hp, fmin, tpe, Trials, STATUS_OK, space_eval
-from xgboost import XGBRegressor
+from sklearn.linear_model import LogisticRegression
+# from xgboost import XGBRegressor
+
+
+def find_best_model(xtrain,ytrain,xtest,ytest):
+
+    # defining space 
+    space ={
+        'penalty':hp.choice('penalty',['l1','l2','elasticnet']),
+        'l1_ratio':hp.uniform('l1_ratio',0,1),
+        'solver':hp.choice('solver',['lbfgs', 'liblinear', 'newton-cg', 'newton-cholesky', 'sag', 'saga'])
+    }
+
+    # function to train the model and evaluate the best one
+    def objective(params):
+        penalty = params['penalty']
+        l1_ratio = params['l1_ratio']
+        solver = params['solver']
+
+        with mlflow.start_run():
+
+            lr = LogisticRegression(penalty=penalty,l1_ratio=l1_ratio,solver=solver)
+            model=lr.fit(xtrain,ytrain)
+
+            pred = model.predict(xtest)
+
+            f1_score = f1_score(ytest,pred)
+            auc_roc = roc_auc_score(ytest,pred)
+
+            mlflow.log_param('penalty',penalty)
+            mlflow.log_param('l1_ratio',l1_ratio)
+            mlflow.log_param('solver',solver)
+
+            mlflow.log_metric('f1_score',f1_score)
+            mlflow.log_metric('auc_roc',auc_roc)
+
+        return auc_roc            
+    
+    # findind the best model
+    best = fmin(fn=objective,
+                space=space,
+                algo=tpe.suggest,
+                trials=Trials()
+                max_evals=10)
+    
+    # logging the model
+    with mlflow.start_run():
+        mlflow.log_params(best)
+
+        # train the model with best parameters
+        final_model = LogisticRegression(penalty=best['penalty'],l1_ratio=best['l1_ratio'],solver=best['solver'],
+                                         random_state= 42)
+        final_model.fit(xtrain,ytrain)
+
+        mlflow.sklearn.log_model(final_model,"Best_Model")
+
+    return final_model
+
+
+def save_model(model, output_path):
+    # Save the trained model to the specified output path
+    joblib.dump(model, output_path + "/model.joblib")
+
+
+def main():
+
+    curr_dir = pathlib.Path(__file__)
+
+    home_dir = curr_dir.parent.parent.parent
+
+    input_file = sys.argv[1]
+    data_path = home_dir.as_posix() + input_file
+
+    output_path = home_dir.as_posix() + '/models'
+    pathlib.Path(output_path).mkdir(parents=True , exist_ok= True)
+
+    TARGET = 'Class'
+    train_features = pd.read_csv(data_path + '/train.csv')
+    X = train_features.drop(TARGET, axis=1)
+    y = train_features[TARGET]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+    trained_model = find_best_model(X_train, y_train, X_test, y_test)
+
+    save_model(trained_model, output_path)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
